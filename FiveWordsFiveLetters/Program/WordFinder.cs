05bf44ebf8cea25128a2172aca4694790cd6c752
MN-House4it <mn@house4it.com>
2024-05-24 11:04:41 +0200

    Notes

--- FiveWordsFiveLetters/Program/WordFinder.cs
+++ FiveWordsFiveLetters/Program/WordFinder.cs
@@ -12,7 +12,7 @@ namespace WordProgram
 {
     public class WordFinder
     {
-        // Events to return status and time
+        // Events to return status and time to the front end
         public event Action<int> ProgressChanged;
         public event Action<Stopwatch> TimeChanged;
         
@@ -21,21 +21,19 @@ namespace WordProgram
 
 
         int wordLength;
-        //int alphabetLetterRemaning;
         int numberOfWords;
-
         int numberOfAlphabeticWordsToRun = 1;
 
         
         List<List<string>> result = new List<List<string>>();
-  
 
+        // Used to calculate the percentage of completion to send to the front end
         int doneCount = 0;
         int doneFinalCount = 0;
 
-
         Dictionary<int, string> dictionary = new Dictionary<int, string>();
         List<int> bitList = new List<int>();
+
         Dictionary<int, int> alphabetDictionary = new Dictionary<int, int>();
         int[][] alphabetLists;
         List<KeyValuePair<int, int>> sortedAlphabet;
@@ -47,55 +45,62 @@ namespace WordProgram
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
-
+            // Setting the target word length
             this.wordLength = length;
 
+            // Reading all lines from the specified file
             string[] readFile = File.ReadAllLines(filepath);
 
+            // Loop through each line in the read file
             for (int i = 0; i < readFile.Count(); i++)
             {
+                // Check if the current word meets the specified length and doesn´t contains double letters
                 if (CheckLength(readFile[i], length) && CheckDoubleLetter(readFile[i]))
                 {
+                    // Convert the valid word to its bit representation and process it
                     FromStringToBits(readFile[i]);
                 }
             }
 
-
-
-
+            // Sorting the alphabet dictionary by values in ascending order
             sortedAlphabet = (
                 from entry in alphabetDictionary
                 orderby entry.Value ascending
                 select entry
             ).ToList();
 
+            // Initialize an array to hold lists of bits grouped by sorted alphabet
             alphabetLists = new int[sortedAlphabet.Count][];
 
             for (int i = 0; i < sortedAlphabet.Count; i++)
             {
+                // Filter bitList to include only those bits that match the current sorted alphabet key
                 alphabetLists[i] = bitList.Where(x => (x & sortedAlphabet[i].Key) != 0).ToArray();
+                // Update bitList to remove the processed bits
                 bitList = bitList.Where(x => (x & sortedAlphabet[i].Key) == 0).ToList();
             }
-            WordsArray(length);
+            Calculate(length);
 
-
+            // Stop the timer and invoke the TimeChanged event with the elapsed time, for the front end to display
             stopwatch.Stop();
             TimeChanged?.Invoke(stopwatch);
 
-
+            // Invoke ProgressChanged for the front end to mark as complete 
             ProgressChanged?.Invoke(100);
 
+            // Return the result of the run
             return result;
         }
 
-        private void WordsArray(int length)
+        private void Calculate(int length)
         {
             // Sends 0% to the GUI to start showing the progressbar
             ProgressChanged?.Invoke(0);
             
+            // Calculate the start values like the number of words per combination
             CalculateStartValues();
 
-  
+            // Rund the recursive function to gather all the possible word combinations
             WordCombinationFinder(0, 0, new Stack<int>());
         }
 
@@ -121,14 +126,15 @@ namespace WordProgram
 
 
 
-
-        // Best 16 sec debug 10 sec release
+        // Best 12 sec debug 4.5 sec release
         private void WordCombinationFinder(int usedBits, int pointer, Stack<int> matchedBits)
         {
             if (sortedAlphabet == null) return;
 
+            // Check if the matchedBits stack has reached the required number of words
             if (matchedBits.Count == numberOfWords)
             {
+                // Thread-safe addition of the matched combination to the result list
                 lock (result)
                 {
                     result.Add(matchedBits.Select(bit => dictionary[bit]).ToList());
@@ -136,55 +142,60 @@ namespace WordProgram
                 return;
             }
 
+            // Calculate the maximum index to iterate over the alphabet lists
             int maxIndex = alphabetLists.Length - (alphabetLists.Length - numberOfAlphabeticWordsToRun - matchedBits.Count * numberOfWords);
+
+            // Iterate over the alphabet lists starting from the given pointer
             for (int letter = pointer; letter < maxIndex && alphabetLists.Length > letter && alphabetLists[letter] != null; letter++)
             {
+                // Skip if the current letter's key has already been used
                 if ((sortedAlphabet[letter].Key & usedBits) != 0) continue;
 
+                // If no bits are matched yet, use parallel processing for better performance on the first of the five words
                 if (matchedBits.Count == 0)
                 {
                     Parallel.ForEach(alphabetLists[letter], bit =>
                     {
+                        // Skip if the current bit overlaps with usedBits
                         if ((bit & usedBits) != 0) return;
 
+                        // Create a new stack for matched bits and push the current bit
                         var newMatchedBits = new Stack<int>(matchedBits);
                         newMatchedBits.Push(bit);
 
+                        // Recursive call to find further combinations for the next words
                         WordCombinationFinder(usedBits | bit, letter + 1, newMatchedBits);
 
+                        // Increment the done count safely and update progress
                         Interlocked.Increment(ref doneCount);
                         int progress = (int)(((double)doneCount / doneFinalCount) * 100);
                         ProgressChanged?.Invoke(progress);
                     });
-
                 }
                 else
                 {
-                    //foreach (var bit in alphabetLists[letter].Where(bit => (bit & usedBits) == 0))
-                    //{
-                    //    matchedBits.Push(bit);
-                    //    WordMatches(usedBits | bit, letter + 1, matchedBits);
-                    //    matchedBits.Pop();
-                    //}
-
+                    // Sequential processing for subsequent levels of recursion to find the words possible after the first one
                     var bitsList = alphabetLists[letter];
                     int count = bitsList.Count();
 
                     for (int i = 0; i < count; i++)
                     {
                         var bit = bitsList[i];
+
+                        // Proceed if the current bit does not overlap with usedBits
                         if ((bit & usedBits) == 0)
                         {
+                            // Push the current bit to the stack
                             matchedBits.Push(bit);
+                            // Recursive call to find further combination
                             WordCombinationFinder(usedBits | bit, letter + 1, matchedBits);
+                            // Pop the bit after returning from recursion
                             matchedBits.Pop();
                         }
                     }
                 }
             }
         }
-
-
 
 
 
@@ -219,11 +230,13 @@ namespace WordProgram
             }
         }
 
+
         // Test if the word length is correct
         private bool CheckLength(string word, int length)
         {
             return word.Length == length;
         }
+
 
         // Test if the word is the right length
         private bool CheckDoubleLetter(string word)
