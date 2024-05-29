1dd347d609e8b871503ccf81a1613ddd444fd428
Gayvery <lokelindolsen@gmail.com>
2024-05-29 08:39:09 +0200

    Teenager

---
+++ PeopleVillage/PeopleVilleTeenager/Creator/TeenagerVillagerCreator.cs
@@ -0,0 +1,39 @@
+﻿using PeopleVilleEngine.Locations;
+using PeopleVilleEngine;
+using PeopleVilleEngine.Villagers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeopleVilleEngine.Villagers.Creators;
+
+namespace PeopleVilleTeenager.Create
+{
+    public class TeenagerVillagerCreator : IVillagerCreator
+    {
+
+        public bool CreateTeenagerVillager(Village village)
+        {
+
+
+            if (village.Villagers.Count(v => v.IsTeenager()) > village.Villagers.Count* 0.3)
+                return false; //No more the 30% can be Teenager
+
+            var random = RNG.GetInstance();
+                 return false; //1 of 10 chance to create a pension
+
+            //Teenager start 12
+            var Teenager = new TeenagerVillager(village, random.Next(12, 19));
+            ////Add to village
+            village.Villagers.Add(Teenager);
+            return true;
+        }
+ 
+
+        public bool CreateVillager(Village village)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PeopleVillage/PeopleVilleVillagerHomeless/obj/Debug/net7.0/PeopleVilleVillagerHomeless.AssemblyInfo.cs
+++ PeopleVillage/PeopleVilleVillagerHomeless/obj/Debug/net7.0/PeopleVilleVillagerHomeless.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeopleVilleVillagerHomeless")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a033629bf6162b992864ce111ec9e1a69ae7279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeopleVilleVillagerHomeless")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeopleVilleVillagerHomeless")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
